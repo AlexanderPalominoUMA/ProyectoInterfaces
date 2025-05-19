37255b0d9ba5e9d6fb317943b827ca9aaa32e033
Dalibex <dalibexyt@gmail.com>
2025-05-19 14:23:53 +0200

    Reglas Update
    Añadidas reglas al menú con explicación detallada y un gif de ejemplo (falta añadir la de emplatado cuando esté terminada)

--- src/providers/ReglasProvider.jsx
+++ src/providers/ReglasProvider.jsx
@@ -5,9 +5,10 @@ import { Modal, Button } from 'react-bootstrap';
 function ReglasModal({ show, close }) {
   const [currentPage, setCurrentPage] = useState(0);
   const [rules] = useState([
-    { text: "Regla 1: el pepe", image: "/images/regla1.jpg" },
-    { text: "Regla 2: el pepe x2", image: "/images/regla2.jpg" },
-    { text: "Regla 3: el pepe x3", image: "/images/regla3.jpg" }
+    { text: "Estacion 1 (Bechamel): Arrastra para batir la leche con la harina cuando la barra esté en verde, y después elegir uno de los 3 ingredientes (según el pedido del cliente para batirlo) haciendo click en batir hasta completar la barra de progreso", image: "/images/bechamel.gif" },
+    { text: "Estacion 2 (Empanado): Haz click en el bol para sacar las croquetas, mantén pulsado para arrastrar la croqueta, déjala en la harina, huevo y pan rallado en ese orden y luego sueltalas en el hueco para mandarlas a la siguiente estación", image: "/images/empanado.gif" },
+    { text: "Estacion 3 (Fritura): Enciende el aceite haciendo click a las freidoras, y luego vuelve arrastrar las croquetas a las freidoras, cuando su estado de cocción sea el deseado por el cliente haz click nuevamente a la freidora y se sacarán automáticamente", image: "/images/fritura.gif" },
+    { text: "Estacion 4 (Emplatado): Elige la salsa que haya pedido el cliente y pulsa las croquetas para ponerlas en el plato, listo!", image: "/images/emplatado.gif", alt: "Emplatado gif" },
   ]);
 
   const handleNext = () => {
@@ -23,7 +24,7 @@ function ReglasModal({ show, close }) {
   };
 
   return (
-    <Modal centered show={show} onHide={close}>
+    <Modal centered show={show} onHide={close} dialogClassName="wide-modal">
       <Modal.Header closeButton>
         <Modal.Title>Reglas del Juego</Modal.Title>
       </Modal.Header>
@@ -31,7 +32,7 @@ function ReglasModal({ show, close }) {
         <div className="game-rules">
           <div className="rule-content">
             <p className="rule-text">{rules[currentPage].text}</p>
-            {/* <img src={rules[currentPage].image} alt="Regla" className="rule-image" /> */}
+            {<img src={rules[currentPage].image} alt="Regla" className="rule-image" />}
           </div>
 
           <div className="navigation-buttons">
