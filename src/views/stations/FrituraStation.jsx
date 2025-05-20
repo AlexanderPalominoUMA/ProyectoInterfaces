0ddc44f56e072df0b29f6e0435528f57aad7e5c0
Dalibex <dalibexyt@gmail.com>
2025-05-20 18:40:17 +0200

    Desactivar fritura
    Faltaba hacer lo mismo para fritura una vez terminada la estacion

--- src/views/stations/FrituraStation.jsx
+++ src/views/stations/FrituraStation.jsx
@@ -43,7 +43,14 @@ function FrituraStation() {
     }
   }, [croquetasServidas, finishedStations]);
 
-
+  useEffect(() => {
+  if (
+    croquetasServidas.length === pedido.cantidad &&
+    !finishedStations.includes("fritura")
+  ) {
+    setFinishedStations([...finishedStations, "fritura"]);
+  }
+  }, [croquetasServidas, pedido.cantidad, finishedStations]);
 
   useEffect(() => {
     const handleMove = (e) => {
