82ab0c5d471545f98969360857660aabecaea4ea
Lewas88 <Lemaprieto@uma.es>
2025-06-01 15:15:50 +0200

    arreglo navbar

--- src/views/Game.jsx
+++ src/views/Game.jsx
@@ -22,6 +22,7 @@ function Game() {
   const { id } = useParams();
   const { openSettings } = useSettings();
   const location = useLocation();
+  estadoNavbar = false; // Reseteamos el estado de la navbar al cargar el componente
 
   const [finishedStations, setFinishedStations] = useState([]);
   const [pedido, setPedido] = useState(null);
